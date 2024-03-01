e9900b2dca5913fdecfc7fcff79bd52a6eb87517
Micaela <mleivaachaval@ufl.edu>
2024-03-01 14:12:54 -0500

    updates

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+/* var elementIntro = document.getElementById("intro");
+let btn = document.getElementById("btn");
+var elementPage = document.getElementById("page");
+
+elementPage.style.display = "none";
+
+//esto hace que se muestre la pagina
+btn.onclick = function showPage() {
+    elementPage.style.display = "block";
+    elementIntro.style.display = "none";
+};
+*/
+
+
+var elementbiei = document.getElementById("main");
+
+var elementBuenosAiresPhoto = document.getElementById("buenosAiresPhoto");
+let elementBuenosAiresText = document.getElementById("buenosAiresText");
+
+elementbiei.style.display = none;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* aca arranca el mapa
+let mapVar = L.map('map2').setView([-19.704684, -59.429373], 3.4);
+  L.tileLayer('https://api.mapbox.com/styles/v1/micaleiva1/clb1hq2p8000314mjjwoj5h2t/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWljYWxlaXZhMSIsImEiOiJjbGFsazYzazgwNjZhM29xYzg2djRpdWV0In0.mI9xq8pXSbYhFTvax_TOmA', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+  }).addTo(mapVar);
+
+  for (let i = 0; i < places.length; i++) {
+    L.marker([places[i].latitude, places[i].longitude])
+    .bindPopup('<h3>' + places[i].placeName + '</h3>' + '<p>' + places[i].description + '</p>')
+    .addTo(mapVar);
+  }
+aca termina el mapa */
